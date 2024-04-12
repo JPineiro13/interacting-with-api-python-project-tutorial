4f9579012e6c4b71420ac28eb653774eea8086b8
Jesús Piñeiro <150048586+JPineiro13@users.noreply.github.com>
2024-04-12 18:05:26 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,14 +2,33 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+con = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret))
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+artist_id = "4NJhFmfw43RLBLjQvxDuRS"
+
+response = sp.artist_top_tracks("4NJhFmfw43RLBLjQvxDuRS")
+if response:
+  # We keep the "tracks" object of the answer
+  tracks = response["tracks"]
+  # We select, for each song, the data we are interested in and discard the rest
+  tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+
+print(tracks_df.head(3))
